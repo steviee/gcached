0fac22f0cbe57c231d38dcaa1ee7c9024b2981d8
Stephan Eberle <stephan.eberle@xing.com>
2017-06-12 15:48:00 +0200

    Added circleci config

--- item.go
+++ item.go
@@ -6,6 +6,6 @@ type Item struct {
 	Key         string    `json:"key"`
 	Value       string    `json:"value"`
 	CreatedAt   time.Time `json:"created"`
-	ProlongedAt time.Time `json:"created"`
+	ProlongedAt time.Time `json:"prolongedAt"`
 	TTL         int       `json:"ttl"`
 }
